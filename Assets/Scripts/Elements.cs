8fb232831ee4e757a9b1ce6054699367d6de49ff
Milos Zlatanovic <milos.m.zlatanovic@gmail.com>
2021-11-05 09:54:05 +0100

    Initial commit

---
+++ Assets/Scripts/Elements.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Sript is add to all falling elements prefab
+public class Elements : MonoBehaviour
+{
+    public int pointsElement;
+    public int intexElements;
+   
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            HudMenu.instance.SetSpriteElements(intexElements);
+          
+            HudMenu.instance.BonusSet(intexElements);        
+           
+            // Taking points and sending to HUD
+            HudMenu.instance.AddPoints(pointsElement);
+            HudMenu.instance.SetElementFields();
+            Destroy(gameObject, 0.03f);
+        }
+    }
+}
---
+++ Assets/Scripts/EntrySceneMenu.cs
@@ -0,0 +1,16 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EntrySceneMenu : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI highScore;
+
+    void Start()
+    {
+        highScore.text = PlayerPrefs.GetInt("Score", 0).ToString();
+    }
+    // Button OnClick() Load GamePlay Scene
+    public void PlayGame() => SceneManager.LoadScene(1, LoadSceneMode.Single);
+}
